59d071d46ae9ac5b430ce9cd3029cae2d992798d
anjanasom <anjanasom05@gmail.com>
2025-02-11 01:32:55 +0530

    added sign up flash messages
    if y'all don't like it we can get rid of it

--- website/auth.py
+++ website/auth.py
@@ -1,5 +1,5 @@
 #for backend user authentification
-from flask import Blueprint, render_template, request
+from flask import Blueprint, render_template, request, flash
 
 auth = Blueprint('auth', __name__)
 
@@ -12,11 +12,23 @@ def login():
 def logout():
     return render_template("index.html")
 
-@auth.route('/sign-up', methods=['GET', 'POST'])
+@auth.route('/signup', methods=['GET', 'POST'])
 def sign_up():
     if request.method == 'POST':
         email = request.form.get('email')
         name = request.form.get('name')
         password = request.form.get('password')
         confirm_password = request.form.get('confirm_password')
+
+        if len(email) < 4:
+            flash("Email must be at least 4 characters.", category = "error")
+        elif len(name) < 2:
+            flash("Name must be at least 2 characters.", category = "error")
+        elif password != confirm_password:
+            flash("Passwords don't match.", category = "error")
+        elif len(password) < 8:
+            flash("Password must be at least 8 characters.", category = "error")
+        else:
+            # add user to db
+            flash("Account created!", category = "success")
     return render_template("signup.html")
